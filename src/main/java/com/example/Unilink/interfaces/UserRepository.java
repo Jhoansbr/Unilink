0dfcd3820e97bcdef84555b0d229d7204a8be25d
Jhoan Blanco <jhoansbr2004@gmail.com>
2025-05-22 17:59:14 -0500

    Prueba 5:59

--- src/main/java/com/example/Unilink/interfaces/UserRepository.java
+++ src/main/java/com/example/Unilink/interfaces/UserRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Unilink.Modelo.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmailAndContrasena(String email, String contrasena);
-    Optional<User> findByEmail(String email);
+
 }
