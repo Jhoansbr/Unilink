b2739e38fbd8d2e341056e59fc38278da7230f8e
Jhoan Blanco <jhoansbr2004@gmail.com>
2025-05-22 06:40:34 -0500

    Cambio User Service

--- src/main/java/com/example/Unilink/service/UserService.java
+++ src/main/java/com/example/Unilink/service/UserService.java
@@ -1,30 +1,7 @@
 package com.example.Unilink.service;
-import com.example.Unilink.Modelo.User;
-import com.example.Unilink.interfaces.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public User registerUser(User user) {
-        user.setContrasena(passwordEncoder.encode(user.getContrasena()));
-        return userRepository.save(user);
-    }
-
-    public Optional<User> authenticateUser(String email, String password) {
-        Optional<User> userOpt = userRepository.findByEmail(email);
-        if (userOpt.isPresent() && passwordEncoder.matches(password, userOpt.get().getContrasena())) {
-            return userOpt;
-        }
-        return Optional.empty();
-    }
 }
