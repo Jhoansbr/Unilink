9fc2fdf70745f6c187ab51cb7209b4135f8b2291
Jhoan Blanco <jhoansbr2004@gmail.com>
2025-05-22 17:16:32 -0500

    Comentar Post/login

--- src/main/java/com/example/Unilink/Controller/LoginController.java
+++ src/main/java/com/example/Unilink/Controller/LoginController.java
@@ -18,42 +18,45 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
-
-	@Autowired
-	private UserRepository usuarioRepository;
-
-	@Autowired
-	private JwtUtil jwtUtil;
-
-	@PostMapping("/login")
-	public String login(@RequestParam String email,
-			@RequestParam String password,
-			HttpServletResponse response,
-			RedirectAttributes redirectAttributes) {
-
-		Optional<User> userOpt = usuarioRepository.findByEmailAndContrasena(email, password);
-
-		if (userOpt.isPresent()) {
-			String token = jwtUtil.generateToken(email);
-
-			Cookie cookie = new Cookie("jwt", token);
-			cookie.setHttpOnly(true);
-			cookie.setPath("/");
-			cookie.setMaxAge(3600); // 1 hora
-			response.addCookie(cookie);
-			System.out.print("Esta entrando en el controlador ");
-			int rol = userOpt.get().getRol();
-			if (rol == 1) {
-				return "redirect:/admin/dashboard";
-			} else {
-				return "redirect:/cliente/inicio";
-			}
-		} else {
-				redirectAttributes.addFlashAttribute("error", "Credenciales inválidas");
-				return "redirect:/entrar";
-			}
-		}
-
+	/*
+	 * @Autowired
+	 * private UserRepository usuarioRepository;
+	 * 
+	 * @Autowired
+	 * private JwtUtil jwtUtil;
+	 * 
+	 * 
+	 * @PostMapping("/login")
+	 * public String login(@RequestParam String email,
+	 * 
+	 * @RequestParam String password,
+	 * HttpServletResponse response,
+	 * RedirectAttributes redirectAttributes) {
+	 * 
+	 * Optional<User> userOpt = usuarioRepository.findByEmailAndContrasena(email,
+	 * password);
+	 * 
+	 * if (userOpt.isPresent()) {
+	 * String token = jwtUtil.generateToken(email);
+	 * 
+	 * Cookie cookie = new Cookie("jwt", token);
+	 * cookie.setHttpOnly(true);
+	 * cookie.setPath("/");
+	 * cookie.setMaxAge(3600); // 1 hora
+	 * response.addCookie(cookie);
+	 * System.out.print("Esta entrando en el controlador ");
+	 * int rol = userOpt.get().getRol();
+	 * if (rol == 1) {
+	 * return "redirect:/admin/dashboard";
+	 * } else {
+	 * return "redirect:/cliente/inicio";
+	 * }
+	 * } else {
+	 * redirectAttributes.addFlashAttribute("error", "Credenciales inválidas");
+	 * return "redirect:/entrar";
+	 * }
+	 * }
+	 */
 		@GetMapping("/logout")
 		public String logout(HttpServletResponse response) {
 			Cookie cookie = new Cookie("jwt", null);
